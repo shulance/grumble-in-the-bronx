b9a13e5146edea496723e0e7c2044b4489f74901
Sam Hulance <sam.hulance@googlemail.com>
2014-09-09 05:04:28 -0500

    Adding highlights widget

---
+++ widgets/highlights/bvSDKFramework/models/varsContainers.js
@@ -0,0 +1,46 @@
+
+
+var bvTargetContainer = {
+
+	/********** headers - page, section **********/
+
+	"header" : {
+
+		"universal" : {
+			"section" : "[data-bv-target='header-section-universal']", // section header container
+		},
+
+	},
+
+	/********** general display items **********/
+
+	"ugc" : {
+		
+		"universal" : {
+			"container-pullquote-widget" : "[data-bv-target='ugc-container-pullquote-widget-universal']", // pullquote highlight widget
+			"pullquote" : "[data-bv-target='ugc-pullquote-universal']", // pullquote ugc container
+
+			"container-widget" : "[data-bv-target='ugc-container-widget-universal']", // entire ugc widget
+			"container-group" : "[data-bv-target='ugc-container-group-universal']", // ugc group
+			"container-individual" : "[data-bv-target='ugc-container-individual-universal']", // ugc individual item
+			"nickname" : "[data-bv-target='ugc-nickname-universal']", // nickname container
+			"location" : "[data-bv-target='ugc-location-universal']", // location container
+		},
+
+		"review" : {
+			"rating-overall" : "[data-bv-target='ugc-rating-overall-review']", // overall rating module
+		},
+
+	},
+}
+
+var bvObjectVariables = {
+	"container" : {
+		"rating-star" : "[data-bv-container='rating-star']", // star rating container
+		"rating-star-filled" : "[data-bv-container='rating-star-filled']", // filled star container
+		"rating-star-image-filled" : "[data-bv-container='rating-star-image-filled']", // filled star image
+		"rating-star-unfilled" : "[data-bv-container='rating-star-unfilled']", // unfilled star container
+		"rating-star-image-unfilled" : "[data-bv-container='rating-star-image-unfilled']", // unfilled star image
+		"rating-star-text" : "[data-bv-container='rating-star-text']", // star rating text
+	},
+}
